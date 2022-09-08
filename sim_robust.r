52f591492c70cb9f21fba98e61f6664984ab6265
Sarah Herbert <ms.sherbert@gmail.com>
2022-09-08 17:04:14 +1200

    Update sim_robust.r

--- sim_robust.r
+++ sim_robust.r
@@ -14,28 +14,46 @@ p <- 0.23 #Individual detection probability for primary period
 
 #--- Simulate a single scenario under Ricker population growth model ---#
 
-# Simulate Poisson-distributed local abundance across sites
-y <- N <- matrix(NA, M, T)
+# Simulate Poisson-distributed local abundance across sites for each primary period
+N <- matrix(NA, M, T)
 N[,1] <- rpois(M, lambda)
+
     for(t in 2:T) {
         mu <- N[,t-1]*exp(r*(1-N[,t-1]/K))
         N[,t] <- rpois(M, mu)
     }
-	
-	
-y[] <- rbinom(M*T, N, p) # Create simulated counts based on undelying model
+
+# Expand matrix to add in secondary periods
+y <- n <- matrix(rep(as.numeric(t(N)), each = J), nrow = nrow(N), byrow = TRUE)
+
+# Create simulated counts based on underlying abundance matrix and detection probabilty
+y[] <- rbinom(M*J*T, n, p) 
+
+#--- Optional if you want to see what your simulated data looks like ---#
+
+par(mfrow=c(1,2))
+TotAbund <- colSums(N) 
+plot(TotAbund ~ c(1:T), xlab = "Year", ylab="Total abundance")
+TotCounts <- colSums(y) 
+Year <- c(1:T)
+Year <- rep(Year,each=J)
+plot(TotCounts ~ Year, ylab="N individuals per secondary period")
+
+par(mfrow=(c(1,1)) #Re-sets the graphics window back to one pane
 
 #--- Prepare simulated data set for unmarked analysis ---#
 umf <- unmarkedFramePCO(y = y, numPrimary=T)
 summary(umf)
-plot(umf) #optional if you want a visual representation of the count data for each year and site
+#plot(umf) #if you want a visual representation of the count data for each year and site
+
 
 #--- Fit model and extract parameter estimates ---#
-m1 <- pcountOpen(~1, ~1, ~1, ~1, umf, K=30,dynamics="ricker") # K should be higher than maximum N(it) and sufficiently high to not constrain estimates of N(it).
+m1 <- pcountOpen(~1, ~1, ~1, ~1, umf, K=30,dynamics="trend") # K should be higher than maximum N(it) and sufficiently high to not constrain estimates of N(it).
+# Note that in runs of previous versions of this code, models with dynamics="ricker" returned singular Hessian matrices. 
 summary(m1) #Provides parameter estimates and the associated standard errors and p-values.
 
 #--- Power analysis ---#
 # Set desired effect sizes to pass to coefs
-effect_sizes <- list(gamma)
+effect_sizes <- list(gamma) #To fix - this is missing any refernce to model m1!
 # Run power analysis and look at summary
 pa <- powerAnalysis(m1, coefs=effect_sizes, alpha=0.05)
